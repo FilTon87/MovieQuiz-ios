473dc2d0ade3d72a89f017b7a174fd78461da544
Filipchuk Anton <fanton.qm@gmail.com>
2023-06-25 15:14:19 +0300

    Верстка интерфейса и выводим первый вопрос на экран

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,9 +1,56 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+
+    @IBOutlet weak private var imageView: UIImageView!
+    @IBOutlet weak private var textLabel: UILabel!
+    @IBOutlet weak private var counterLabel: UILabel!
+    
+    struct QuizQuestion {
+        let image: String
+        let test: String
+        let correctAnswer: Bool
+    }
+    
+    struct QuizStepViewModel {
+      let image: UIImage
+      let question: String
+      let questionNumber: String
+    }
+    
+    private let quiestions = [
+        QuizQuestion(image: "The Godfather", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Dark Knight", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Kill Bill", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Avengers", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Deadpool", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Green Knight", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Old", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "The Ice Age Adventures of Buck Wild", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Tesla", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Vivarium", test: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+    ]
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(image: UIImage(named: "\(model.image)") ?? UIImage(), question: "\(model.test)", questionNumber: "\(currentQuestionIndex + 1) / \(quiestions.count)")
+        return questionStep
+    }
+    
+    private func show(quizstep: QuizStepViewModel) {
+        counterLabel.text = quizstep.questionNumber
+        imageView.image = quizstep.image
+        textLabel.text = quizstep.question
+    }
+
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        let currentQuestion = quiestions[currentQuestionIndex]
+        show(quizstep: convert(model: currentQuestion))
     }
 }
 
